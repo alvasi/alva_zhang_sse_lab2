0617f1409d261f74b29f51b84a55e8cae3d62704
Alva Si <alva.si19@imperial.ac.uk>
2023-10-19 14:21:34 +0100

    adjusted input colour

--- api/app.py
+++ api/app.py
@@ -9,5 +9,5 @@ def hello_world():
 def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
-    input_fav_colour = request.form.get("colour")
-    return render_template("hello.html", name=input_name, age=input_age)   
+    input_colour = request.form.get("colour")
+    return render_template("hello.html", name=input_name, age=input_age, colour=input_colour)   
