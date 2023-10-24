35af2f8db4536536b4900785bb9a72bccd685c86
Zhang Zhang <zz3823@imperial.ac.uk>
2023-10-24 11:28:21 +0100

    Added a number guessing game

--- api/app.py
+++ api/app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
+
+secret_number = None
+num_g = 7
+range = 100
 
 
 @app.route("/")
@@ -21,14 +26,43 @@ def submit():
 @app.route("/query", methods=["GET"])
 def query():
     query_param = request.args.get("q")
-    result = process_query(query_param)
-    return result
+    result = handle_guess(query_param)
+    return render_template("result.html", result=result)
 
 
-def process_query(q):
-    if q == "dinosaurs":
-        return "Dinosaurs ruled the Earth 200 million years ago"
-    elif q == "asteroids":
-        return "Unknown"
+# def process_query(q):
+#     if q == "dinosaurs":
+#         return "Dinosaurs ruled the Earth 200 million years ago"
+#     elif q == "asteroids":
+#         return "Unknown"
+#     else:
+#         return "Unknown"
+
+
+def handle_guess(guess):
+    global num_g
+    if guess.isdigit():
+        guess = int(guess)
+        if guess < secret_number and num_g > 1:
+            num_g -= 1
+            return "Higher. Number of remaining guesses is " + str(num_g) + "."
+        elif guess > secret_number and num_g > 1:
+            num_g -= 1
+            return "Lower. Number of remaining guesses is " + str(num_g) + "."
+        elif guess == secret_number:
+            return "Correct!"
+        elif num_g <= 1:
+            return "No remaining guesses."
     else:
-        return "Unknown"
+        return "Invalid query. Please enter a number."
+
+
+def new_game():
+    global secret_number, num_g, range
+    secret_number = random.randrange(0, range)
+    print("New game! Range is [0, " + str(range) + ").")
+    print("Number of remaining guesses is " + str(num_g) + ".")
+    print(" ")
+
+
+new_game()
--- api/app_test.py
+++ api/app_test.py
@@ -1,12 +1,34 @@
-from app import process_query
+# from app import process_query
 
 
-def test_knows_about_dinosaurs():
-    assert (
-        process_query("dinosaurs") ==
-        "Dinosaurs ruled the Earth 200 million years ago"
-    )
+# def test_knows_about_dinosaurs():
+#     assert (
+#         process_query("dinosaurs") ==
+#         "Dinosaurs ruled the Earth 200 million years ago"
+#     )
 
 
-def test_does_not_know_about_asteroids():
-    assert process_query("asteroids") == "Unknown"
+# def test_does_not_know_about_asteroids():
+#     assert process_query("asteroids") == "Unknown"
+
+from app import handle_guess
+
+
+def test_handle_guess_lower():
+    assert handle_guess(50) == "Higher. Number of remaining guesses is 6."
+
+
+def test_handle_guess_higher():
+    assert handle_guess(70) == "Lower. Number of remaining guesses is 5."
+
+
+def test_handle_guess_correct():
+    assert handle_guess(60) == "Correct!"
+
+
+def test_handle_guess_invalid():
+    assert handle_guess("abc") == "Invalid query. Please enter a number."
+
+
+def test_handle_guess_no_remaining_guesses():
+    assert handle_guess(80) == "No remaining guesses."
