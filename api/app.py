5027831510cbd7e5967a3c4561f1d5f5bfbde0a4
Alva Si <alva.si19@imperial.ac.uk>
2023-10-24 13:49:45 +0100

    fixing coverage check

--- api/app.py
+++ api/app.py
@@ -41,13 +41,13 @@ def handle_guess(guess):
     if isinstance(guess, int):
         guess = int(guess)
         if guess < secret_number and num_g > 1:
-            num_g -= 1
             return "Too low!. Number of remaining guesses is "
             + str(num_g) + "."
-        elif guess > secret_number and num_g > 1:
             num_g -= 1
+        elif guess > secret_number and num_g > 1:
             return "Too high!. Number of remaining guesses is "
             + str(num_g) + "."
+            num_g -= 1
         elif guess == secret_number:
             game_message = new_game()
             return game_message
