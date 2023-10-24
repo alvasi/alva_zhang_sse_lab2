26dc0f229a585c99d919fedca7682b88f1fcb0b9
Alva Si <alva.si19@imperial.ac.uk>
2023-10-24 13:25:19 +0100

    stylistic formatting

--- api/app.py
+++ api/app.py
@@ -42,11 +42,11 @@ def handle_guess(guess):
         guess = int(guess)
         if guess < secret_number and num_g > 1:
             num_g -= 1
-            return "Too low!. Number of remaining guesses is " 
+            return "Too low!. Number of remaining guesses is "
             + str(num_g) + "."
         elif guess > secret_number and num_g > 1:
             num_g -= 1
-            return "Too high!. Number of remaining guesses is " 
+            return "Too high!. Number of remaining guesses is "
             + str(num_g) + "."
         elif guess == secret_number:
             new_game()
