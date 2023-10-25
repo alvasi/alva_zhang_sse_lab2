88a1d4fdce5801aa2493a7fe5167d353d81175e0
Alva Si <alva.si19@imperial.ac.uk>
2023-10-25 20:00:15 +0100

    changing tests

--- api/app_test.py
+++ api/app_test.py
@@ -9,14 +9,11 @@ class TestGuessGame(TestCase):
         return app
 
     def test_guess_equals_num(self):
-        with self.client.session_transaction() as session:
-            session['num'] = 50
-            session['chances'] = 7
-        rv = self.client.get('/guess', query_string={'guess': 50})
-        self.assert200(rv)
-        self.assert_template_used('result.html')
-        self.assertIn('Hurray! You got it in 0 steps!',
-                      rv.data.decode())
+    with self.client.session_transaction() as session:
+        session['num'] = 50
+        session['chances'] = 7
+    rv = self.client.get('/guess', query_string={'guess': 50})
+    self.assertStatus(rv, 302)
 
     def test_guess_less_than_num(self):
         with self.client.session_transaction() as session:
@@ -39,14 +36,11 @@ class TestGuessGame(TestCase):
                       rv.data.decode())
 
     def test_no_chances_left(self):
-        with self.client.session_transaction() as session:
-            session['num'] = 50
-            session['chances'] = 1
-        rv = self.client.get('/guess', query_string={'guess': 25})
-        self.assert200(rv)
-        self.assert_template_used('result.html')
-        self.assertIn('Phew! You lost the game. You are out of chances',
-                      rv.data.decode())
+    with self.client.session_transaction() as session:
+        session['num'] = 50
+        session['chances'] = 1
+    rv = self.client.get('/guess', query_string={'guess': 25})
+    self.assertStatus(rv, 302)
 
 
 if __name__ == '__main__':
