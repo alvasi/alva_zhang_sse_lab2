90f9ef214950d63b214b97188942a73d5c9d0384
Alva Si <alva.si19@imperial.ac.uk>
2023-10-30 16:55:25 +0000

    change function

--- api/app_test.py
+++ api/app_test.py
@@ -78,9 +78,10 @@ def test_multiply():
 
 
 def test_squarecube():
-    assert (
-        process_query(
-             "Which of the following numbers is "
-             "both a square and a cube: 16, 1, "
-             "1315, 125, 1758, 2639, 3720?") == ["1", "125"]
+    result = process_query(
+        "Which of the following numbers is "
+        "both a square and a cube: 16, 1, "
+        "1315, 125, 1758, 2639, 3720?"
     )
+    expected_result = [1, 125]
+    assert result == expected_result
