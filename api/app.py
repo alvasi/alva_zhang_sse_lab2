8f76cab7dd2d04401d1c460378bba77b42f618b4
Alva Si <alva.si19@imperial.ac.uk>
2023-10-30 15:54:30 +0000

    fixed comment and add coverage

--- api/app.py
+++ api/app.py
@@ -94,6 +94,7 @@ def process_query(q):
 def find_largest_number(question):
     if "Which of the following numbers is the largest: " in question:
         numbers_str = question.split(": ")[1]
-        numbers = [int(num.strip("?").strip()) for num in numbers_str.split(",")]
+        numbers = [int(num.strip("?").strip())
+                   for num in numbers_str.split(",")]
         return str(max(numbers))
     return None
