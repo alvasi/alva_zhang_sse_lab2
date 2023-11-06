4ebf4fb33c4addccdc27450c25538b4a8588e8b4
Alva Si <alva.si19@imperial.ac.uk>
2023-11-06 16:33:47 +0000

    trying to fix flake

--- api/app.py
+++ api/app.py
@@ -177,7 +177,7 @@ def gitquery():
         repos = response.json()
         for repo in repos:
             commit_response = requests.get(
-                f"https://api.github.com/users/{input_username}/
+                f"https://api.github.com/users/{input_username}/\
                 {repos['name']}/commits"
             )
             if commit_response.status_code == 200:
