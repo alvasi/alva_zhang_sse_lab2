e9f5c1a23dec83008511a17ae20ddce284cd151b
Zhang Zhang <zz3823@imperial.ac.uk>
2023-11-07 17:28:18 +0000

    Added information of followers and following and a contribution calender. Added error.html.

--- api/app.py
+++ api/app.py
@@ -7,55 +7,55 @@ import requests
 
 
 app = Flask(__name__)
-app.secret_key = 'some_secret_key'  # set a secret key for the session
+app.secret_key = "some_secret_key"  # set a secret key for the session
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    session['num'] = random.randint(1, 100)
-    session['chances'] = 7
-    result = session.pop('result', None)
-    return render_template('index.html', result=result)
+    session["num"] = random.randint(1, 100)
+    session["chances"] = 7
+    result = session.pop("result", None)
+    return render_template("index.html", result=result)
 
 
-@app.route('/guess', methods=['GET', 'POST'])
+@app.route("/guess", methods=["GET", "POST"])
 def guess_game():
-    if request.method == 'POST':
-        guess = int(request.form.get('guess'))
+    if request.method == "POST":
+        guess = int(request.form.get("guess"))
     else:
-        guess = int(request.args.get('guess'))
+        guess = int(request.args.get("guess"))
 
-    num = session['num']
-    chances = session['chances']
+    num = session["num"]
+    chances = session["chances"]
 
     if num is None or chances is None:
-        session['num'] = random.randint(1, 100)
-        session['chances'] = 7
-        return redirect(url_for('index'))
+        session["num"] = random.randint(1, 100)
+        session["chances"] = 7
+        return redirect(url_for("index"))
 
     if guess == num:
-        result = 'Hurray! You got it in {} steps!'.format(7 - chances)
-        session['result'] = result
-        return redirect(url_for('index'))
+        result = "Hurray! You got it in {} steps!".format(7 - chances)
+        session["result"] = result
+        return redirect(url_for("index"))
 
     elif guess < num:
-        result = 'Your number is less than the random number'
+        result = "Your number is less than the random number"
     else:
-        result = 'Your number is greater than the random number'
+        result = "Your number is greater than the random number"
 
     chances -= 1
-    session['chances'] = chances
+    session["chances"] = chances
 
     if chances == 0:
-        result = 'Phew! You lost the game. You are out of chances'
-        session['result'] = result
-        return redirect(url_for('index'))
+        result = "Phew! You lost the game. You are out of chances"
+        session["result"] = result
+        return redirect(url_for("index"))
 
-    return render_template('result.html', result=result)
+    return render_template("result.html", result=result)
 
 
-if __name__ == '__main__':
-    app.template_folder = 'templates'
+if __name__ == "__main__":
+    app.template_folder = "templates"
     app.run()
 
 
@@ -103,7 +103,7 @@ def process_query(q):
 
 
 def add(text):
-    numbers = re.findall(r'\d+', text)
+    numbers = re.findall(r"\d+", text)
 
     if len(numbers) == 2:
         num1 = int(numbers[0])
@@ -114,7 +114,7 @@ def add(text):
 
 
 def minus(text):
-    numbers = re.findall(r'\d+', text)
+    numbers = re.findall(r"\d+", text)
 
     if len(numbers) == 2:
         num1 = int(numbers[0])
@@ -127,15 +127,14 @@ def minus(text):
 def find_largest_number(question):
     if "Which of the following numbers is the largest: " in question:
         numbers_str = question.split(": ")[1]
-        numbers = [int(num.strip("?").strip())
-                   for num in numbers_str.split(",")]
+        numbers = [int(num.strip("?").strip()) for num in numbers_str.split(",")]
         return str(max(numbers))
     return None
 
 
 def multiply(text):
     # Use regular expressions to find the two integers in the sentence.
-    numbers = re.findall(r'\d+', text)
+    numbers = re.findall(r"\d+", text)
 
     if len(numbers) == 2:
         # Convert the found numbers to integers and calculate their product.
@@ -150,7 +149,7 @@ def multiply(text):
 
 def find_square_and_cube_numbers(text):
     # Use regular expressions to find integers in the sentence.
-    numbers = re.findall(r'\d+', text)
+    numbers = re.findall(r"\d+", text)
     square_and_cube_numbers = []
 
     for number_str in numbers:
@@ -163,8 +162,8 @@ def find_square_and_cube_numbers(text):
 
 def is_square_and_cube(number):
     # Check if a number is both a square and a cube.
-    square_root = number ** 0.5
-    cube_root = number ** (1/3)
+    square_root = number**0.5
+    cube_root = number ** (1 / 3)
     return square_root.is_integer() and cube_root.is_integer()
 
 
@@ -176,28 +175,46 @@ def datetimeformat(value, format="%Y-%m-%d %H:%M:%S"):
 @app.route("/gitquery", methods=["GET"])
 def gitquery():
     input_username = request.args.get("gitquery")
-    user_response = requests.get(
-        f"https://api.github.com/users/{input_username}")
+    user_response = requests.get(f"https://api.github.com/users/{input_username}")
     repos_response = requests.get(
-        f"https://api.github.com/users/{input_username}/repos")
-
-    if user_response.status_code == 200 and repos_response.status_code == 200:
+        f"https://api.github.com/users/{input_username}/repos"
+    )
+    followers_response = requests.get(
+        f"https://api.github.com/users/{input_username}/followers"
+    )
+    following_response = requests.get(
+        f"https://api.github.com/users/{input_username}/following"
+    )
+    if (
+        user_response.status_code == 200
+        and repos_response.status_code == 200
+        and followers_response.status_code == 200
+        and following_response.status_code == 200
+    ):
         user_data = user_response.json()
         repos = repos_response.json()
+        followers = followers_response.json()
+        following = following_response.json()
 
         for repo in repos:
             commit_response = requests.get(
-                f"https://api.github.com/repos/{repo['full_name']}/commits")
+                f"https://api.github.com/repos/{repo['full_name']}/commits"
+            )
             if commit_response.status_code == 200:
                 commits = commit_response.json()[:1]
-                repo['latest_commits'] = commits
+                repo["latest_commits"] = commits
                 for commit in commits:
-                    commit['commit']['author']['date'] = (
-                        commit['commit']['author']['date']
-                    )
+                    commit["commit"]["author"]["date"] = commit["commit"]["author"][
+                        "date"
+                    ]
 
         return render_template(
-            "gitquery.html", gitquery=input_username,
-            user=user_data, repos=repos)
+            "gitquery.html",
+            gitquery=input_username,
+            user=user_data,
+            repos=repos,
+            followers=followers,
+            following=following,
+        )
     else:
         return render_template("error.html")
